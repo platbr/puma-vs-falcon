6531079b97efc40b819dbfd7a022b2bd32b00042
Wagner Caixeta <wagner@baladapp.com.br>
2024-09-26 11:08:24 -0300

    two

--- test2.sh
+++ test2.sh
@@ -4,24 +4,6 @@ sleep 1
 curl http://localhost:3000/simulate/block &
 curl http://localhost:3000/simulate/block &
 curl http://localhost:3000/simulate/block &
-curl http://localhost:3000/simulate/block &
-curl http://localhost:3000/simulate/block &
-curl http://localhost:3000/simulate/block &
-curl http://localhost:3000/simulate/block &
-curl http://localhost:3000/simulate/block &
-curl http://localhost:3000/simulate/block &
-curl http://localhost:3000/simulate/block &
-curl http://localhost:3000/simulate/block &
-curl http://localhost:3000/simulate/block &
-curl http://localhost:3000/simulate/block &
-curl http://localhost:3000/simulate/block &
-curl http://localhost:3000/simulate/block &
-curl http://localhost:3000/simulate/block &
-curl http://localhost:3000/simulate/block &
-curl http://localhost:3000/simulate/block &
-curl http://localhost:3000/simulate/block &
-curl http://localhost:3000/simulate/block &
-curl http://localhost:3000/simulate/block &
 sleep 1
 echo "Running 1 non-blocking request.."
 curl http://localhost:3000/simulate/non-block
