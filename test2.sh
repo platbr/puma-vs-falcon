36f4f803c831e764fb63261d73e0afc18016ef1c
Wagner Caixeta <wagner@baladapp.com.br>
2024-09-26 11:07:29 -0300

    two

--- test2.sh
+++ test2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "Running multiple blocking requests.."
+echo "Running 3 blocking requests.."
+sleep 1
 curl http://localhost:3000/simulate/block &
 curl http://localhost:3000/simulate/block &
 curl http://localhost:3000/simulate/block &
@@ -20,3 +21,7 @@ curl http://localhost:3000/simulate/block &
 curl http://localhost:3000/simulate/block &
 curl http://localhost:3000/simulate/block &
 curl http://localhost:3000/simulate/block &
+curl http://localhost:3000/simulate/block &
+sleep 1
+echo "Running 1 non-blocking request.."
+curl http://localhost:3000/simulate/non-block
